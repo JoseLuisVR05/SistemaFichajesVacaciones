1c7a64f207aaa32ef85d2906ec4de31799b73305
Jose Luis Valderrama <jvalderrama@alsina.com>
2026-01-21 12:14:29 +0100

    DB: Crear migración inicial con Employee y TimeEntry

--- SistemaFichajesVacaciones.Infrastructure/AppDbContext.cs
+++ SistemaFichajesVacaciones.Infrastructure/AppDbContext.cs
@@ -11,4 +11,64 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Employee> Employees => Set<Employee>();
+     public DbSet<TimeEntry> TimeEntries => Set<TimeEntry>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuración de Employee
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId);
+            
+            entity.Property(e => e.EmployeeCode)
+                .IsRequired()
+                .HasMaxLength(50);
+            
+            entity.HasIndex(e => e.EmployeeCode)
+                .IsUnique();
+            
+            entity.Property(e => e.FullName)
+                .IsRequired()
+                .HasMaxLength(200);
+            
+            entity.Property(e => e.Email)
+                .IsRequired()
+                .HasMaxLength(255);
+            
+            entity.HasIndex(e => e.Email)
+                .IsUnique();
+
+            // Relación jerárquica (empleado-manager)
+            entity.HasOne(e => e.Manager)
+                .WithMany(e => e.Subordinates)
+                .HasForeignKey(e => e.ManagerEmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        // Configuración de TimeEntry
+        modelBuilder.Entity<TimeEntry>(entity =>
+        {
+            entity.HasKey(e => e.TimeEntryId);
+            
+            entity.Property(e => e.EntryType)
+                .IsRequired()
+                .HasMaxLength(20);
+            
+            entity.Property(e => e.Source)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Relación con Employee
+            entity.HasOne(e => e.Employee)
+                .WithMany()
+                .HasForeignKey(e => e.EmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Índice compuesto para búsquedas eficientes
+            entity.HasIndex(e => new { e.EmployeeId, e.EventTime });
+        });
+    }
+
 }
